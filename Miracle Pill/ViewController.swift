fc0d15b316acfa117568711ae9f2d71b9c5d2e54
Kimar Arakaki Neves <kimar@kimar.eng.br>
2017-01-13 00:55:17 -0800

    Implementation of a simple screen to understand iOS flow
    - Using the keyboard
    - Using scrollview
    - Upside down rotation support
    - Textfield, buttons and screen segues

--- Miracle Pill/ViewController.swift
+++ Miracle Pill/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, UITextFieldDelegate {
 
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var successImage: UIImageView!
+    
+    let states = ["Arkansas", "California", "Maine", "New York", "Texas", "Alaska"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
+        scrollView.isScrollEnabled = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,60 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func stateButtonPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
 
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
+
+    @IBAction func buyButtonPressed(_ sender: AnyObject) {
+        for view in self.view.subviews as [UIView] {
+            view.isHidden = true
+        }
+        successImage .isHidden = false
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.tag != -1 {
+            scrollView.setContentOffset(CGPoint(x: 0, y: 250), animated: true)
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField.tag != -1 {
+            scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+        }
+    }
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask{
+        return UIInterfaceOrientationMask.all
+    }
 }
 
